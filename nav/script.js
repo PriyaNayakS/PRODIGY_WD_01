836a3fe7890e447d371f88a876764a5b4ff18e60
PriyaNayakS <141226108+PriyaNayakS@users.noreply.github.com>
2024-09-29 16:01:43 +0530

    Add files via upload

---
+++ nav/script.js
@@ -0,0 +1,9 @@
+
+window.onscroll = function() {
+    var navbar = document.getElementById("navbar");
+    if (window.scrollY > 0) {
+        navbar.classList.add("nav-fix");
+    } else {
+        navbar.classList.remove("nav-fix");
+    }
+};
